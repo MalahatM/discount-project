97d352a742fc074b5db9dea8261e297564c5f294
your_username <malahat.m82@gmail.com>
2025-08-10 15:13:30 +0200

    add initial products list

---
+++ src/products.js
@@ -0,0 +1,12 @@
+export const products = [
+  { id: 1, name: "shoe", price: 200, off_price: 120 },
+  { id: 2, name: "dumbbells", price: 150, off_price: 90 },
+  { id: 3, name: "rope", price: 50, off_price: 35 },
+  { id: 4, name: "wristband", price: 30, off_price: 25 },
+  { id: 5, name: "medicine ball", price:80, off_price:50 },
+  { id: 6, name: "foam roller", price:100, off_price:70},
+  { id: 7, name: "barbell", price:250, off_price:200},
+  { id: 8, name:"treadmill", price:400, off_price:300},
+  { id: 9, name:"sporteswear",price:280, off_price:160 },
+  { id: 10, name:"tennis",price:130, off_price:90}
+];
